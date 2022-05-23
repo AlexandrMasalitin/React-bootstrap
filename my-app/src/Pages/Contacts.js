dae85bee758b6e04222e269feadec610b6f0a13b
AlexandrMasalitin <masalitindev@gmail.com>
2022-05-24 01:04:00 +0300

    modified files in App.css and Contact.js

--- my-app/src/Pages/Contacts.js
+++ my-app/src/Pages/Contacts.js
@@ -33,6 +33,40 @@ export default class Contacts extends Component {
           </Form.Group>
           <Button variant="success" type="submit">Submit</Button>
         </Form>
+      <div className="position-items">
+      <Button color="primary" className="position-relative"
+        >Mails
+        <span
+          className=" translate-middle badge rounded-pill bg-secondary px-1"
+        >
+          +99
+          <span className="visually-hidden">unread messages</span>
+        </span>
+      </Button>
+      <Button color="dark" className="position-relative">
+        Marker
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          className=" translate-middle bi bi-caret-down-fill"
+          fill="#343a40"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"
+          />
+        </svg>
+      </Button>
+      <Button color="primary" className="position-relative">
+        Alerts
+        <span
+          className=" translate-middle badge border border-light rounded-circle bg-danger p-2"
+        >
+          <span className="visually-hidden">unread messages</span>
+        </span>
+      </Button>
+    </div>
 
         <Placement />
       </Container>
